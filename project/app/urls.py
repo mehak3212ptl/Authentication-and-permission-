4e6b38433435db822f4d42083707814a4246cd24
mehak <mehakpatelattitude03@gmail.com>
2024-08-12 18:39:09 +0530

    endpoints token

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path,include
 from .views import *
 from rest_framework import routers
-
+from rest_framework.authtoken.views import obtain_auth_token 
 router = routers.DefaultRouter()
 router.register(r'users', Studentviewset, basename='user')
 
 
 urlpatterns =[
     path('',include(router.urls)),
-    path('api-auth/', include('rest_framework.urls'))
+    # path('api-auth/', include('rest_framework.urls'))
+    path('api-token-auth/',obtain_auth_token)
 ]
